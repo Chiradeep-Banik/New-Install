81f3c7e2a36c7fd2b198d21dae915bee4137a279
Chiradeep-Banik <chiradeep13@gmail.com>
2022-01-15 23:54:39 +0000

    made the install script

--- install_deb.sh
+++ install_deb.sh
@@ -1,21 +1,49 @@
 #!/bin/sh
-echo "Please enter the username"
-read varuser
-id -u $varuser > /dev/null
-if [ $? -ne 0 ]; then
-        echo "The user is not valid"
-        exit -1
-fi
-apt -y install build-essential libx11-dev libxinerama-dev libxft-dev \
+
+echo "This is the installation script....\n"
+
+echo "Installing the softwares using apt\n"
+sudo apt -y install build-essential libx11-dev libxinerama-dev libxft-dev \
                git wget xinit xorg xserver-xorg-dev xserver-xorg x11-xserver-utils \
-               curl compton feh
-               
-cd /dwm-6.3/
-make clean install
-make clean
-cd ../st-0.8.5
-make clean install
-make clean
-cd ../dmenu-5.0
-make clean install
-make clean
+               curl compton feh vim
+
+echo "Copied the bashrc and vimrc\n"
+cp .bashrc ~/.bashrc
+cp .vimrc ~/.vimrc
+
+echo "Installed vim plug\n"
+sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+echo "Make the suckless directory\n"
+cd ~
+mkdir suckless
+cd suckless
+
+#DWM -- Tiling window manager
+echo "Starting dwm\n"
+git clone --depth 1 git://git.suckless.org/dwm
+cd dwm
+sudo make clean
+sudo make clean install
+cd ..
+
+# ST -- simple terminal
+echo "Starting st\n"
+git clone https://github.com/Chiradeep-Banik/st.git
+cd st
+sudo make clean
+sudo make clean install
+cd ..
+
+# dmenu -- application launcher
+echo "Starting dmenu\n"
+git clone https://github.com/Chiradeep-Banik/dmenu.git
+cd dmenu
+sudo make clean
+sudo make clean install
+cd ~
+
+# Adding the xinitrc
+echo exec dwm >> .xinitrc
